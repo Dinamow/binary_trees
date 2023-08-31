e4ffdc9eb40701b64fbc3311ec07020921d5bd8b
Dinamow <meemoo102039@gmail.com>
2023-08-31 07:20:01 +0000

    fix betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,11 +1,15 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_sibling - get the sib
  *
+ * @node - the req node
+ *
+ * Return: sib node of the node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if(!node || !node->parent)
+	if (!node || !node->parent)
 		return (NULL);
 	if (node->parent->left == node)
 		return (node->parent->right);
