157edaded4698d39c0e4b549ea19e953812e9917
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-08-31 07:26:51 +0000

    fix a betty bug with 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,7 +20,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
-	
+
 	if (tree == NULL)
 		return (0);
 	left_height = binary_tree_height(tree->left);
