dfdc0497b122376dd9eb5fc42ab45508a8ca8291
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-08-31 07:49:15 +0000

    fix is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,6 +2,7 @@
 
 
 size_t binary_tree_nodes_copy(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is full
@@ -12,7 +13,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_nodes_copy(tree) % 2);
+	return (binary_tree_nodes_copy(tree) % 2 &&
+			binary_tree_height(tree->left) ==
+			binary_tree_height(tree->right));
 }
 
 /**
@@ -32,4 +35,21 @@ size_t binary_tree_nodes_copy(const binary_tree_t *tree)
 	count += binary_tree_nodes_copy(tree->right);
 	count += 1;
 	return (count);
+}
+
+/**
+ * binary_tree_height - Measure the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ * Return: Height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
